dca5618ab60e1cd5b04cbdd6f2fd3869f17c08f7
Mohamed Kdidi <mohamedkdidi@gmail.com>
2022-03-07 15:47:32 -0500

    Upate main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,26 @@ resource "azurerm_resource_group" "kdidi" {
   name     = "kdidi"
   location = "eastus2"
 }
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "kdidi-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.kdidi.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.kdidi.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
+}
+
+# Create a docker container
+resource "docker_container" "ubuntu" {
+  name  = "ubuntu-latest"
+  image = docker_image.ubuntu.latest
+}
+  
