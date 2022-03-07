178a0890b301c613cd59ee32c78a357b351f9fa6
Mohamed Kdidi <mohamedkdidi@gmail.com>
2022-03-07 15:43:20 -0500

    Upate main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,16 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.98.0"
+    }
+  }
   # Store Terraform state in Azure Storage
   backend "azurerm" {
     resource_group_name  = "rgkdiditfstates"
     storage_account_name = "satfstatedevops"
     container_name       = "contterraformgithubactions"
     key                  = "keyterraformgithubactions.tfstate"
-  }
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.98.0"
-    }
   }
 }
 
@@ -25,26 +25,3 @@ resource "azurerm_resource_group" "kdidi" {
   name     = "kdidi"
   location = "eastus2"
 }
-
-# Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "kdidi-vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = "eastus2"
-  resource_group_name = azurerm_resource_group.kdidi.name
-}
-
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.kdidi.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
-}
-
-# Create a docker container
-resource "docker_container" "ubuntu" {
-  name  = "ubuntu-latest"
-  image = docker_image.ubuntu.latest
-}
-  
