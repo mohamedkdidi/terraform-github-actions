e04a00c322dc1d1a0426cd9b4699d94c4c24689d
Mohamed Kdidi <mohamedkdidi@gmail.com>
2022-03-07 18:07:30 -0500

    Add Azure Container Group instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,8 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 # Create Resource Group
-resource "azurerm_resource_group" "kdidi" {
-  name     = "kdidi"
+resource "azurerm_resource_group" "group" {
+  name     = "kdidi-resource-group"
   location = "eastus2"
 }
 
@@ -25,13 +25,46 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "kdidi-vnet"
   address_space       = ["192.168.0.0/16"]
   location            = "eastus2"
-  resource_group_name = azurerm_resource_group.kdidi.name
+  resource_group_name = azurerm_resource_group.group.name
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.kdidi.name
+  name                 = "kdidi-subnet"
+  resource_group_name  = azurerm_resource_group.group.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "192.168.0.0/24"
+}
+
+# Create a docker container
+resource "azurerm_container_group" "container" {
+  name                = "kdidi-container"
+  location            = azurerm_resource_group.group.location
+  resource_group_name = azurerm_resource_group.group.name
+  ip_address_type     = "public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "sidecar"
+    image  = "mcr.microsoft.com/azuredocs/aci-tutorial-sidecar"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+  tags = {
+    environment = "testing"
+  }
 }
