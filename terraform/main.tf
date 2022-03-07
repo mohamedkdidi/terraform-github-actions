c1baae559d8cd8f364b10930d42f5662eb8b1c01
Mohamed Kdidi <mohamedkdidi@gmail.com>
2022-03-06 21:04:49 -0500

    Upate main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,25 +6,10 @@ terraform {
     container_name       = "contterraformgithubactions"
     key                  = "keyterraformgithubactions.tfstate"
   }
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=2.91.0"
-    }
-  }
 }
 
 provider "azurerm" {
   features {}
-
-  # More information on the authentication methods supported by
-  # the AzureRM Provider can be found here:
-  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
-
-  # subscription_id = "..."
-  # client_id       = "..."
-  # client_secret   = "..."
-  # tenant_id       = "..."
 }
 
 data "azurerm_client_config" "current" {}
