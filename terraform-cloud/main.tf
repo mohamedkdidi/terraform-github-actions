81f34b0e0f340ef39f6ca7617dd983525cc9b04e
Mohamed Kdidi <mohamedkdidi@gmail.com>
2022-03-09 00:50:45 -0500

    Change deprecated argument

--- terraform-cloud/main.tf
+++ terraform-cloud/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rsgroup.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
+  address_prefixes       = "192.168.0.0/24"
 }
 
 # Create a docker container
